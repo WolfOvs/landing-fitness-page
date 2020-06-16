cb1266d62d5dc6e72e85fe8d5fb87c9cc014ad9d
WolphOvs <davidechiozzi87@gmail.com>
2020-06-16 16:38:16 +0200

    progress

--- dist/js/detects.js
+++ dist/js/detects.js
@@ -1,2 +1,17 @@
 /*! project-name v0.0.1 | (c) 2020 YOUR NAME | MIT License | http://link-to-your-git-repo.com */
 console.log('detects.js file loaded');
+
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
--- src/js/detects.js
+++ src/js/detects.js
@@ -1,1 +1,16 @@
 console.log('detects.js file loaded');
+
+let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
